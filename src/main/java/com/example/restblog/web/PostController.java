6988e2d0f263ca61dda4e64e9e320719a27bc356
Micah Wood <micahswood7@gmail.com>
2022-04-14 16:56:59 -0500

    edited PostController.java, PostIndex.js, and router.js

--- src/main/java/com/example/restblog/web/PostController.java
+++ src/main/java/com/example/restblog/web/PostController.java
@@ -14,9 +14,9 @@ public class PostController {
     @GetMapping
     private List<Post> getAll(){
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", "Blah blah blah"));
-        posts.add(new Post(2L, "Post 2", "Blah blah blah blah"));
-        posts.add(new Post(3L, "Post 3", "Blah blah blah blah blah"));
+        posts.add(new Post(1L, "Dream Post", "My dad awoke to himself saying 500 million dollars."));
+        posts.add(new Post(2L, "Dinner Post", "I had 10 piece chicken nuggets, large fries, and large chocolate shake from McDonald's."));
+        posts.add(new Post(3L, "Painting Post", "My dad painted for my little sibling's birthday that's on thursday."));
         return posts;
     }
 
@@ -25,5 +25,20 @@ public class PostController {
     public Post getById(@PathVariable Long postId){
         Post post = new Post(postId, "Post " + postId, "Blah blah blah");
         return post;
+    }
+
+    @PostMapping
+    private void createPost(@RequestBody Post newPost){
+        System.out.println("Ready to add post: " + newPost);
+    }
+
+    @PutMapping("{postId}")
+    private void updatePost(@PathVariable Long postId, @RequestBody Post updatedPost){
+        System.out.println("Updatig post : " + postId + " with " + updatedPost);
+    }
+
+    @DeleteMapping("{postId}")
+    private void deletePost(@PathVariable Long postId){
+        System.out.println("Post has been deleted: " + postId);
     }
 }
--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,12 +1,93 @@
+import createView from "../createView.js";
+import addLoginEvent from "../auth";
+
+const POST_URI = "http://localhost:8081/api/posts";
+
 export default function PostIndex(props) {
     return `
+    <div class="container-fluid">
         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
-            <div>
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+            <div id="posts-container">
+                ${props.posts.map(post => {
+                return `<h3 id="title-${post.id}">${post.title}</h3>
+                 <p id="content-${post.id}">${post.content}</p>
+                 <a href="#" class="edit-link" data-id="${post.id}">Edit</a>
+                 <a href="#" class="delete-link" data-id="${post.id}">Delete</a>`
+                
+    }).join('')} 
             </div>
+            
+            <div class="container" id="add-posts">
+               <form id="add-posts-form">
+                <div class="mb-3">
+                    <label for="add-posts-title" class="form-label">Title</label>
+                    <input type="text" class="form-control" id="add-posts-title" placeholder="Enter Title">
+                </div>
+                <div class="mb-3">
+                    <label for="add-posts-content" class="form-label">Content</label>
+                    <input type="text" class="form-control" id="add-posts-content" placeholder="Enter Content">
+                </div>
+                <button id="add-posts-button" type="button" class="btn btn-primary mb-3">Save Post</button>
+               </form>
+           </div>
         </main>
+    </div>
     `;
+}
+
+export function PostsEvent() {
+    createAddPostListener();
+    //TODO: add edit post listener function
+    EditListener();
+    //TODO: add delete post listener function
+    deleteListener();
+}
+
+function createAddPostListener() {
+    console.log("adding add post listener")
+    $("#add-post-button").click(function (){
+        const title = $("#add-post-title").val();
+        const content = $("#add-post-content").val();
+        const newPost = {
+            title,
+            content
+        }
+        console.log("Ready to add: ");
+        console.log(newPost);
+
+        const request = {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newPost)
+        }
+        fetch(POST_URI, request)
+            .then( res => {
+                console.log(res.status)
+            }).catch(error => {
+            console.log(error)
+        }).finally(() => {
+            createView("/posts")
+        });
+    });
+}
+
+function EditListener(){
+    $(".edit-link").click(function (){
+        const postId = $(this).data("id");
+        const postTitle = $("#title-" + postId).val();
+        const postContent = $("#content-" + postId).val();
+        console.log(postId);
+        $("#add-post-title").val(postTitle);
+        $("#add-post-content").val(postContent);
+    });
+}
+
+function deleteListener(){
+    $(".delete-link").click(function (){
+        const postId = $(this).data("id")
+
+    })
 }
